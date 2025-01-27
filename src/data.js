6ad1286d8245bfa6e091299c9071c69e8bce1687
Maksim <msurguy@gmail.com>
2025-01-26 22:57:24 -0800

    converted to handlebars

--- src/data.js
+++ src/data.js
@@ -1,5 +1,19 @@
 export default [
   {
+    src: "assets/interactive_installation.jpg",
+    alt: "Interactive Installation at Seattle Creative Code",
+    title: "Interactive Installation at Seattle Creative Code",
+    link: "https://www.instagram.com/p/DEdZXtsyrj-/?img_index=1",
+    linkText: "View on Instagram"
+  },
+  {
+    src: "assets/drawingbots_plotted_art.png",
+    alt: "Geometric artwork drawn with AxiDraw drawing robot",
+    title: "Algorithmic Art drawn with a robot",
+    link: "https://www.instagram.com/p/CsEi9DOpwkn/",
+    linkText: "View on Instagram"
+  },
+  {
     src: "assets/3dflowfields.png",
     alt: "3D Vector flow fields 002 by Maks Surguy",
     title: "\"3D Vector flow fields 002\"",
@@ -16,14 +30,14 @@ export default [
   {
     src: "assets/nft_museum.jpg",
     alt: "Seattle NFT Museum exhibit by Maks Surguy",
-    title: "NFT artwork Exhibit at Seattle NFT Museum",
+    title: "NFT / Physical artwork Exhibit at Seattle NFT Museum",
     link: "https://www.instagram.com/p/CceltaIMTtK/",
     linkText: "View on Instagram",
   },
   {
     src: "assets/nft_flowfields.png",
     alt: "Vector Field 005 NFT by Maks Surguy",
-    title: "\"Vector Field 005\"",
+    title: "\"Vector Field 005\" NFT",
     link: "https://objkt.com/asset/hicetnunc/1343",
     linkText: "View on OBJKT NFT Marketplace",
   },
